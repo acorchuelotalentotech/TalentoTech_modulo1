8351b14adaccc17bc5a3e60960f5e6853fe08803
acorchuelotalentotech <acorchuelo.talentotechq@gmail.com>
2024-10-02 12:24:10 -0500

    week 3

---
+++ week 3/intro JavaScript/proyecto 1 JS/js/app.js
@@ -0,0 +1,56 @@
+/*console.log("Hola Mundo");
+
+var nombre="juan";
+var apellido="perez";
+const PI = 3.1416;
+let ropa = "camisa";
+
+console.log(nombre);
+console.log(apellido);
+console.log(PI);
+nombre = "andres";
+console.log(nombre);
+//PI = 223;
+console.log(ropa);
+let cantidad = 42;
+
+function saludar(nombre)
+{
+    console.log(nombre);
+}
+
+function quadrado(numero){
+    let quad;
+    quad = numero*numero;
+    return quad;
+    console.log(quad);
+}
+
+let persona = {
+    nombre: "Anna", 
+    edad: 30,
+    Profesion: "Ingeniero"
+}
+let lista=[1, 2, 3, 4, 5, 6]
+console.log(persona);
+console.log(lista[4]);
+saludar("pipe");
+saludar("enrique");
+
+//quadrado(6);
+//quadrado(5);
+//quadrado(9);
+let suma = (quadrado(5)+quadrado(4)+quadrado(3));
+console.log(suma);
+
+let num = 0;
+for(let i = 0; i =< 100; ){
+    num = mum + 1;
+}
+*/
+prom = 0
+i = 1
+while(i<=10){
+    prom *=i
+}
+console.log(prom);
