22fc98f77dc0bd859af763174a5937437b7bc764
acorchuelotalentotech <acorchuelo.talentotechq@gmail.com>
2024-10-09 08:16:34 -0500

    actualizacion

---
+++ week 3/intro JavaScript/funciones/docHtml/js/app.js
@@ -0,0 +1,4 @@
+let elemento = document.getElementById("miElemento");
+console.log(elemento);
+elemento.textContent = "Esto es javascript";
+elemento.style = "color:red";
--- week 3/intro JavaScript/funciones/js/app.js
+++ week 3/intro JavaScript/funciones/js/app.js
@@ -1,17 +1,30 @@
-let edad = 0
-
-aleatorio
-
-function calcularEdad(edad){
-    if (edad <18){
-        console.log("Eres menor de edad");
-    }else if(edad >= 18 && edad < 15){
-        console.log("Eres adulto.");
-    }else {
-        console.log("Eres Mayor")
-    }
-    
+/*
+function aleatorio(min,max){
+    return Math.floor(Math.random() * (max - min + 1)+min )
 }
+let menor = 0
+let mayor = 0
 
-calcularEdad(15);
-console.log(pasa);
+while(menor == 10 || mayor >= 10){
+    let edad = aleatorio(10,30)
+    console.log(edad)
+    let mensaje = (edad >= 18) ? "Mayor" : "Menor";
+    if (mensaje == "Mayor"){
+        mayor = mayor + 1
+    }else if(mensaje == "Menor"){
+        menor = menor + 1
+    }
+   
+}
+//console.log("Mayores de edad =" mayor)
+
+let i = 0
+let mpg = 30
+for(let carro of carros){
+    if (carro.mpg >= mpg){
+        console.log("Marca de carro: " + carro.marca + " mpg: " + carro.mpg )
+        i++
+    }
+}
+console.log("Total de carros = " + i)
+*/
